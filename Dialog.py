85a702516da6b97515a44bfacf1621ec8d3d940b
eason2 <c0972616400@gmail.com>
2017-04-19 11:20:00 +0800

    Create Dialog.py

---
+++ Dialog.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from Tkinter import *
+
+def show(mytext = 'Quit'):
+    win=Tk()
+    win.title(u"系統提示")
+    win.attributes('-fullscreen', True) # 全螢幕
+    def quit2():
+        win.destroy()
+    button2=Button(win, text = mytext, command=quit2)
+    button2.configure(text=mytext,fg = "red")
+    button2["font"] = ("Times",40,'bold italic')
+    button2.place(y=250 ,relx = 0.5,anchor = CENTER)
+    win.mainloop()
+
+if __name__ == "__main__":
+	show("麥克風測試\n測試")
